bcaff058dbb541898b69f08d5dcf0801ac5bdca9
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-20 15:29:26 +0800

    /home/yanghaozhe/.lumo/experiment/graph_comatch.CoMatch/211020.020.58t

--- run_Train_SupCoMatch.py.sh
+++ run_Train_SupCoMatch.py.sh
@@ -2,7 +2,7 @@
 # python Train_SupCoMatch.py --n-labeled 40 --seed 10 --device=0 $@
 # python Train_SupCoMatch.py --n-labeled 40 --seed 10 --device=1 --thr 0.85 $@
 # python Train_SupCoMatch.py --n-labeled 40 --seed 10 --device=1 --thr 0.9 $@
-# python Train_SupCoMatch.py --n-labeled 40 --seed 10 $@
 # python Train_SupCoMatch.py --n-labeled 40 --seed 10 --device=1 $@
+# python Train_SupCoMatch.py --n-labeled 40 --mu 5 --seed 10 $@
 
-python Train_SupCoMatch.py --n-labeled 40 --mu 5 --seed 10 $@
+python Train_SupCoMatch.py --n-labeled 40 --seed 10 $@
