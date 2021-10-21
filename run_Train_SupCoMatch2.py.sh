2bddb7836caaa2993b8f212fc4903f7b37889568
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-21 16:55:57 +0800

    /home/yanghaozhe/.lumo/experiment/supcomatch.CoMatch/211021.010.68t

--- run_Train_SupCoMatch2.py.sh
+++ run_Train_SupCoMatch2.py.sh
@@ -2,5 +2,6 @@
 # python Train_SupCoMatch2.py --n-labeled 40 --seed 10 --device=1 $@
 # python Train_SupCoMatch2.py --n-labeled 40 --seed 10 $@
 # python Train_SupCoMatch2.py --n-labeled 40 --seed 10 --device 1 $@
+# python Train_SupCoMatch2.py --n-labeled 40 --seed 10 --device 0 $@
 
-python Train_SupCoMatch2.py --n-labeled 40 --seed 10 --device 0 $@
+python Train_SupCoMatch2.py --n-labeled 40 --seed 10 --device 2 $@
