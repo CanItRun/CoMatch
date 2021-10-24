08584c46fc2b636c1fa411889e06dcde25d45d0c
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-24 16:45:10 +0800

    /home/yanghaozhe/.lumo/experiment/comatch.comatch/211024.025.10t

--- run_comatch.py.sh
+++ run_comatch.py.sh
@@ -3,5 +3,6 @@
 # python comatch.py --queue=3 $@
 # python comatch.py --queue=4 $@
 # python comatch.py --queue=5 --device=1 $@
+# python comatch.py --queue=5 $@
 
-python comatch.py --queue=5 $@
+python comatch.py --queue=5 --device=4 $@
