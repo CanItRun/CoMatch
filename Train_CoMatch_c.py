f45117c829b9c76c4d0898334b0f8d41966deb04
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-24 18:13:48 +0800

    /home/yanghaozhe/.lumo/experiment/comatch.origin2/211024.018.47t

--- Train_CoMatch_c.py
+++ Train_CoMatch_c.py
@@ -13,7 +13,9 @@ import os
 import sys
 
 import numpy as np
-
+from lumo.contrib.nn.functional import batch_cosine_similarity, masked_log_softmax
+from lumo.contrib.nn.loss import contrastive_loss2
+from lumo.proc.path import cache_dir
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -21,12 +23,99 @@ import torch.nn.functional as F
 from WideResNet import WideResnet
 from datasets.cifar import get_train_loader, get_val_loader
 from utils import accuracy, setup_default_logging, AverageMeter, WarmupCosineLrScheduler
-from lumo import Logger, AvgMeter, Meter
-from lumo.contrib.nn.loss import contrastive_loss2
 
-log = Logger()
-log.add_log_dir('./log_raw')
 import tensorboard_logger
+from lumo import Logger, AvgMeter, Meter
+
+from lumo import TrainerExperiment
+
+exp = TrainerExperiment('comatch.origin2')
+exp.start()
+log = Logger()
+log.add_log_dir(exp.test_root)
+
+
+def contrastive_loss2(query: torch.Tensor, key: torch.Tensor,
+                      memory: torch.Tensor = None,
+                      norm=False,
+                      temperature=0.7,
+                      query_neg=False,
+                      key_neg=True,
+                      qk_graph=None,
+                      qm_graph=None,
+                      eye_one_in_qk=False,
+                      ):
+    """
+    Examples:
+        >>> assert contrastive_loss(a,b,inbatch_neg=False) == contrastive_loss2(a,b,query_neg=False,key_neg=True)
+
+    Args:
+        query: [bq, f_dim]
+        key: [bq, f_dim]
+        memory: [bm, f_dim]
+        norm: bool, normalize feature or not
+        temperature: float
+        query_neg: bool
+        key_neg: bool
+        qk_graph: [bq, bq], >= 0
+        qm_graph: [bq, bm], >= 0
+
+    Returns:
+        loss
+
+    """
+    if memory is None:
+        key_neg = True
+        qm_graph = None
+
+    if memory is not None:
+        key = torch.cat([key, memory])
+
+    if query_neg:
+        key = torch.cat([query, key])
+
+    q_size = query.shape[0]
+
+    if norm:
+        logits = torch.cosine_similarity(query.unsqueeze(1), key.unsqueeze(0), dim=2)
+    else:
+        logits = torch.mm(query, key.t())  # query @ key.t()
+
+    logits /= temperature
+
+    neg_index = torch.ones_like(logits, dtype=torch.bool, device=logits.device)
+
+    _temp_eye_indice = torch.arange(q_size, device=logits.device).unsqueeze(1)
+
+    neg_offset = q_size if query_neg else 0
+    if query_neg:
+        neg_index.scatter_(1, _temp_eye_indice, 0)
+
+    if key_neg:
+        neg_index.scatter_(1, _temp_eye_indice + neg_offset, 0)
+    else:
+        neg_index[:, neg_offset:neg_offset + q_size] = 0
+
+    pos_index = torch.zeros_like(logits, dtype=torch.float, device=logits.device)
+    pos_offset = q_size if query_neg else 0
+
+    # for supervised cs
+    if qk_graph is not None:
+        pos_index[:, :q_size] = qk_graph.float()
+        if query_neg:
+            pos_index[:, q_size:q_size * 2] = qk_graph.float()
+
+    # for supervised cs with moco memory bank
+    if qm_graph is not None:
+        pos_index[:, pos_offset + q_size:] = qm_graph.float()
+
+    if qk_graph is None or eye_one_in_qk:
+        pos_index.scatter_(1, _temp_eye_indice + pos_offset, 1)
+
+    logits_mask = (pos_index > 0) | neg_index
+    loss = -torch.sum(masked_log_softmax(logits, logits_mask, dim=-1) * pos_index, dim=1)
+    loss = (loss / pos_index.sum(1)).mean()
+    return loss
 
 
 def set_model(args):
@@ -80,6 +169,7 @@ def train_one_epoch(epoch,
                     queue_feats,
                     queue_probs,
                     queue_ptr,
+                    dlval
                     ):
     model.train()
     loss_x_meter = AverageMeter()
@@ -98,23 +188,24 @@ def train_one_epoch(epoch,
     avg = AvgMeter()
     for it in range(n_iters):
         meter = Meter()
-        (xs, sxs_0, sxs_1), ys = next(dl_x)
-        (unxs, usxs_0, usxs_1), unys = next(dl_u)
+        (ims_x_weak, ims_x_s0, ims_x_s1), ys = next(dl_x)
+        (ims_u_weak, ims_u_s0, ims_u_s1), unys = next(dl_u)
 
         ys = ys.cuda()
         unys = unys.cuda()
 
         # --------------------------------------
-        bt = xs.size(0)
-        btu = unxs.size(0)
+        bt = ims_x_weak.size(0)
+        btu = ims_u_weak.size(0)
 
-        imgs = torch.cat([xs, sxs_0, sxs_1, unxs, usxs_0, usxs_1], dim=0).cuda()
+        imgs = torch.cat([ims_x_weak, ims_x_s0, ims_x_s1,
+                          ims_u_weak, ims_u_s0, ims_u_s1], dim=0).cuda()
         logits, features = model(imgs)
 
         logits_x, _, _ = logits[:bt * 3].chunk(3)
         logits_u_w, logits_u_s0, logits_u_s1 = torch.split(logits[bt * 3:], btu)
 
-        sup_w_query, _, _ = features[:bt * 3].chunk(3)
+        sup_w_query, sup_query, sup_key = features[:bt * 3].chunk(3)
         un_w_query, un_query, un_key = torch.split(features[bt * 3:], btu)
 
         loss_x = criteria_x(logits_x, ys)
@@ -124,68 +215,190 @@ def train_one_epoch(epoch,
             sup_w_query = sup_w_query.detach()
             un_w_query = un_w_query.detach()
 
-            un_probs = torch.softmax(logits_u_w, dim=1)
+            probs = torch.softmax(logits_u_w, dim=1)
             # DA
-            prob_list.append(un_probs.mean(0))
+            prob_list.append(probs.mean(0))
             if len(prob_list) > 32:
                 prob_list.pop(0)
             prob_avg = torch.stack(prob_list, dim=0).mean(0)
-            un_probs = un_probs / prob_avg
-            un_probs = un_probs / un_probs.sum(dim=1, keepdim=True)
+            probs = probs / prob_avg
+            probs = probs / probs.sum(dim=1, keepdim=True)
 
-            probs_orig = un_probs.clone()
+            probs_orig = probs.clone()
 
             if epoch > 0 or it > args.queue_batch:  # memory-smoothing
                 A = torch.exp(torch.mm(un_w_query, queue_feats.t()) / args.temperature)
                 A = A / A.sum(1, keepdim=True)
-                un_probs = args.alpha * un_probs + (1 - args.alpha) * torch.mm(A, queue_probs)
+                probs = args.alpha * probs + (1 - args.alpha) * torch.mm(A, queue_probs)
 
-            scores, lbs_u_guess = torch.max(un_probs, dim=1)
+            scores, lbs_u_guess = torch.max(probs, dim=1)
             mask = scores.ge(args.thr).float()
 
-            w_query = torch.cat([un_w_query, sup_w_query], dim=0)
+            feats_w = torch.cat([un_w_query, sup_w_query], dim=0)
             onehot = torch.zeros(bt, args.n_classes).cuda().scatter(1, ys.view(-1, 1), 1)
-            w_probs = torch.cat([probs_orig, onehot], dim=0)
+            probs_w = torch.cat([probs_orig, onehot], dim=0)
 
             # update memory bank
             n = bt + btu
-            queue_feats[queue_ptr:queue_ptr + n, :] = w_query
-            queue_probs[queue_ptr:queue_ptr + n, :] = w_probs
+            queue_feats[queue_ptr:queue_ptr + n, :] = feats_w
+            queue_probs[queue_ptr:queue_ptr + n, :] = probs_w
             queue_ptr = (queue_ptr + n) % args.queue_size
 
         # embedding similarity
-        # sim = torch.exp(torch.mm(un_query, un_key.t()) / args.temperature)
-        # sim_probs = sim / sim.sum(1, keepdim=True)
-        #
+        sim = torch.exp(torch.mm(un_query, un_key.t()) / args.temperature)
+        sim_probs = sim / sim.sum(1, keepdim=True)
+
         # pseudo-label graph with self-loop
-        Q = torch.mm(un_probs, un_probs.t())
+        Q = torch.mm(probs, probs.t())
         Q.fill_diagonal_(1)
         pos_mask = (Q >= args.contrast_th).float()
 
         Q = Q * pos_mask
-        Q = Q / Q.sum(1, keepdim=True)
+
+        # Q = Q / Q.sum(1, keepdim=True)
+
+        def sharpen_(x: torch.Tensor, T=0.5):
+            """
+            让概率分布变的更 sharp，即倾向于 onehot
+            :param x: prediction, sum(x,dim=-1) = 1
+            :param T: temperature, default is 0.5
+            :return:
+            """
+            exp.add_tag('sharpen')
+            with torch.no_grad():
+                temp = torch.pow(x, 1 / T)
+                return temp / temp.sum(dim=1, keepdims=True)
+
+        Q = sharpen_(Q)
 
         # contrastive loss
-        loss_contrast = contrastive_loss2(un_query, un_key, temperature=args.temperature, qk_graph=Q)
+        loss_contrast = - (torch.log(sim_probs + 1e-7) * Q).sum(1)
+        loss_contrast = loss_contrast.mean()
+
+        # unsupervised classification loss
+        loss_u = - torch.sum((F.log_softmax(logits_u_s0, dim=1) * probs), dim=1) * mask
+        loss_u = loss_u.mean()
+
+        def comatch_loss():
+            exp.add_tag('Lcs')
+            return contrastive_loss2(un_query, un_key, temperature=args.temperature, qk_graph=Q)
+
+        # contrastive loss
+        loss_contrast = comatch_loss()
 
         # loss_contrast = - (torch.log(sim_probs + 1e-7) * Q).sum(1)
         # loss_contrast = loss_contrast.mean()
 
         # unsupervised classification loss
-        loss_u = - torch.sum((F.log_softmax(logits_u_s0, dim=1) * un_probs), dim=1) * mask
+        loss_u = - torch.sum((F.log_softmax(logits_u_s0, dim=1) * probs), dim=1) * mask
         loss_u = loss_u.mean()
 
-        loss = loss_x + args.lam_u * loss_u + args.lam_c * loss_contrast
+        def choice_(tensor, size=128):
+            return tensor[torch.randperm(len(tensor))[:size]]
 
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
-        lr_schdlr.step()
+        # graph cs2
+        def graph_cs2():
+            # ./log/l.0.2110191754.log
+            exp.add_tag('sup_gcs')
+            memory = queue_feats
+            # memory = torch.cat([un_query, un_key, queue_feats])
+            # pos_memory = memory[torch.randperm(len(memory))[:128]]
+            pos_memory = torch.cat([choice_(un_query),
+                                    choice_(un_key),
+                                    choice_(memory)])
+            pos_memory = choice_(pos_memory, 256)
+            # neg_memory = memory[torch.randperm(len(memory))[:128]]
+            # memory = torch.cat([un_query, un_key, memory])
+
+            anchor = batch_cosine_similarity(sup_query, pos_memory)
+            positive = batch_cosine_similarity(sup_key, pos_memory)
+            # negative = batch_cosine_similarity(sup_key, neg_memory)
+            gqk = ys.unsqueeze(0) == ys.unsqueeze(1)
+            loss = contrastive_loss2(anchor, positive,
+                                     memory=None,
+                                     norm=True,
+                                     temperature=0.2,
+                                     qk_graph=gqk)
+            return loss
+
+        def graph_cs3():
+            # ./log/l.0.2110191754.log
+            exp.add_tag('un_gcs')
+            # memory = queue_feats
+            memory = queue_feats
+            pos_memory = torch.cat([choice_(un_query),
+                                    choice_(un_key),
+                                    choice_(memory)])
+            pos_memory = choice_(pos_memory, 256)
+            # neg_memory = memory[torch.randperm(len(memory))[:512]]
+
+            anchor = batch_cosine_similarity(un_query, pos_memory)
+            positive = batch_cosine_similarity(un_key, pos_memory)
+            # negative = batch_cosine_similarity(un_key, neg_memory)
+
+            # gqk = ys.unsqueeze(0) == ys.unsqueeze(1)
+            loss = contrastive_loss2(anchor, positive,
+                                     memory=None,
+                                     norm=True,
+                                     temperature=0.2,
+                                     qk_graph=Q, eye_one_in_qk=False)
+            return loss
+
+        Lgcs1 = graph_cs2()
+        Lgcs2 = graph_cs3()
+
+        def strategy0():
+            exp.add_tag('loss0')
+            loss = loss_x + args.lam_u * loss_u + loss_contrast
+            return loss
+
+        def strategy1():
+            exp.add_tag('loss1')
+            loss = loss_x + args.lam_u * loss_u + Lgcs1 + Lgcs2 + loss_contrast
+            return loss
+
+        def strategy2():
+            exp.add_tag('loss2')
+            if epoch < 3:
+                loss = loss_x + args.lam_u * loss_u + Lgcs1 + Lgcs2 + loss_contrast
+            else:
+                loss = loss_x + args.lam_u * loss_u + loss_contrast
+            return loss
+
+        def strategy3():
+            exp.add_tag('loss3')
+
+            if epoch < 3:
+                loss = loss_x + args.lam_u * loss_u + Lgcs1 + Lgcs2 + loss_contrast
+            else:
+                loss = loss_x + args.lam_u * loss_u + Lgcs1 * 0.1 + Lgcs2 * 0.1 + loss_contrast
+            return loss
+
+        def strategy4():
+            exp.add_tag('loss4')
+            loss = loss_x + args.lam_u * loss_u + Lgcs1 + Lgcs2
+            return loss
+
+        if args.s == 0:
+            loss = strategy0()
+        elif args.s == 1:
+            loss = strategy1()
+        elif args.s == 2:
+            loss = strategy2()
+        elif args.s == 3:
+            loss = strategy3()
+        elif args.s == 4:
+            loss = strategy4()
+        else:
+            loss = 0
 
         with torch.no_grad():
+            meter.mean.Lall = loss
             meter.mean.Lx = loss_x
             meter.mean.Lu = loss_u
             meter.mean.Lcs = loss_contrast
+            meter.mean.Lgcs1 = Lgcs1
+            meter.mean.Lgcs2 = Lgcs2
             meter.mean.Pm = pos_mask.float().mean()
             meter.mean.Pm = pos_mask.float().mean()
             meter.mean.Ax = (logits_x.argmax(dim=-1) == ys).float().mean()
@@ -193,6 +406,11 @@ def train_one_epoch(epoch,
             meter.mean.Au = (logits_u_w.argmax(dim=-1) == unys).float().mean()
             if mask.bool().any():
                 meter.mean.Aum = (logits_u_w.argmax(dim=-1) == unys)[mask.bool()].float().mean()
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        lr_schdlr.step()
 
         if args.eval_ema:
             with torch.no_grad():
@@ -204,11 +422,10 @@ def train_one_epoch(epoch,
         mask_meter.update(mask.mean().item())
         pos_meter.update(pos_mask.sum(1).float().mean().item())
 
+        avg.update(meter)
         corr_u_lb = (lbs_u_guess == unys).float() * mask
         n_correct_u_lbs_meter.update(corr_u_lb.sum().item())
         n_strong_aug_meter.update(mask.sum().item())
-
-        avg.update(meter)
         log.inline(avg)
         if (it + 1) % 64 == 0:
             t = time.time() - epoch_start
@@ -216,11 +433,18 @@ def train_one_epoch(epoch,
             lr_log = [pg['lr'] for pg in optim.param_groups]
             lr_log = sum(lr_log) / len(lr_log)
 
-            # logger.info("{}-x{}-s{}, {} | epoch:{}, iter: {}. loss_u: {:.3f}. loss_x: {:.3f}. loss_c: {:.3f}. "
-            #             "n_correct_u: {:.2f}/{:.2f}. Mask:{:.3f}. num_pos: {:.1f}. LR: {:.3f}. Time: {:.2f}".format(
-            #     args.dataset, args.n_labeled, args.seed, args.exp_dir, epoch, it + 1, loss_u_meter.avg, loss_x_meter.avg, loss_contrast_meter.avg, n_correct_u_lbs_meter.avg, n_strong_aug_meter.avg, mask_meter.avg, pos_meter.avg, lr_log, t))
-            log.newline()
+            logger.info("{}-x{}-s{}, {} | epoch:{}, iter: {}. loss_u: {:.3f}. loss_x: {:.3f}. loss_c: {:.3f}. "
+                        "n_correct_u: {:.2f}/{:.2f}. Mask:{:.3f}. num_pos: {:.1f}. LR: {:.3f}. Time: {:.2f}".format(
+                args.dataset, args.n_labeled, args.seed, args.exp_dir, epoch, it + 1, loss_u_meter.avg,
+                loss_x_meter.avg, loss_contrast_meter.avg, n_correct_u_lbs_meter.avg, n_strong_aug_meter.avg,
+                mask_meter.avg, pos_meter.avg, lr_log, t))
+
             epoch_start = time.time()
+
+        if (it + 1) % 150 == 0:
+            log.newline()
+            evaluate(model, ema_model, dlval)
+            model.train()
 
     return loss_x_meter.avg, loss_u_meter.avg, loss_contrast_meter.avg, mask_meter.avg, pos_meter.avg, n_correct_u_lbs_meter.avg / n_strong_aug_meter.avg, queue_feats, queue_probs, queue_ptr, prob_list
 
@@ -230,6 +454,7 @@ def evaluate(model, ema_model, dataloader):
 
     top1_meter = AverageMeter()
     ema_top1_meter = AverageMeter()
+
     avg = AvgMeter()
     with torch.no_grad():
         for ims, lbs in dataloader:
@@ -243,6 +468,7 @@ def evaluate(model, ema_model, dataloader):
             top1_meter.update(top1.item())
             meter.sum.A1 = top1
             meter.sum.A5 = top5
+
             if ema_model is not None:
                 logits, _ = ema_model(ims)
                 scores = torch.softmax(logits, dim=1)
@@ -254,9 +480,6 @@ def evaluate(model, ema_model, dataloader):
         log.info(avg)
 
     return top1_meter.avg, ema_top1_meter.avg
-
-
-from lumo.proc.path import cache_dir
 
 
 def main():
@@ -278,6 +501,10 @@ def main():
                         help='train batch size of labeled samples')
     parser.add_argument('--mu', type=int, default=7,
                         help='factor of train batch size of unlabeled samples')
+    parser.add_argument('--device', type=int, default=0,
+                        help='factor of train batch size of unlabeled samples')
+    parser.add_argument('--s', type=int, default=0,
+                        help='策略')
     parser.add_argument('--n-imgs-per-epoch', type=int, default=64 * 1024,
                         help='number of training images for each epoch')
 
@@ -308,11 +535,9 @@ def main():
                         help='number of batches stored in memory bank')
     parser.add_argument('--exp-dir', default='CoMatch', type=str, help='experiment id')
     parser.add_argument('--checkpoint', default='', type=str, help='use pretrained model')
-    parser.add_argument('--device', default=0, type=int, help='use pretrained model')
-    args = parser.parse_args()
 
-    from torch.cuda import set_device
-    set_device(args.device)
+    args = parser.parse_args()
+    torch.cuda.set_device(args.device)
 
     logger, output_dir = setup_default_logging(args)
     logger.info(dict(args._get_kwargs()))
@@ -333,7 +558,9 @@ def main():
     model, criteria_x, ema_model = set_model(args)
     logger.info("Total params: {:.2f}M".format(
         sum(p.numel() for p in model.parameters()) / 1e6))
-
+    from lumo import Trainer, Params
+    trainer = Trainer(Params())
+    trainer.rnd.mark('12')
     dltrain_x, dltrain_u = get_train_loader(
         args.dataset, args.batchsize, args.mu, n_iters_per_epoch, L=args.n_labeled, root=args.root, method='comatch')
     dlval = get_val_loader(dataset=args.dataset, batch_size=64, num_workers=2, root=args.root)
@@ -380,7 +607,8 @@ def main():
     for epoch in range(args.n_epoches):
 
         loss_x, loss_u, loss_c, mask_mean, num_pos, guess_label_acc, queue_feats, queue_probs, queue_ptr, prob_list = \
-            train_one_epoch(epoch, **train_args, queue_feats=queue_feats, queue_probs=queue_probs, queue_ptr=queue_ptr)
+            train_one_epoch(epoch, **train_args, queue_feats=queue_feats, queue_probs=queue_probs, queue_ptr=queue_ptr,
+                            dlval=dlval)
 
         top1, ema_top1 = evaluate(model, ema_model, dlval)
 
---
+++ run_Train_CoMatch_c.py.sh
@@ -0,0 +1,3 @@
+# 
+
+python Train_CoMatch_c.py --n-labeled 40 --device 5 --s 4 $@
