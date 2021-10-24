8dc18276e77e05e45ce19559f6f20421dc3f18ed
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-24 18:15:24 +0800

    /home/yanghaozhe/.lumo/experiment/comatch.origin2/211024.020.06t

--- run_Train_CoMatch_c.py.sh
+++ run_Train_CoMatch_c.py.sh
@@ -1,4 +1,5 @@
 # 
 # python Train_CoMatch_c.py --n-labeled 40 --device 5 --s 4 $@
+# python Train_CoMatch_c.py --n-labeled 40 --device 5 --s 5 $@
 
-python Train_CoMatch_c.py --n-labeled 40 --device 5 --s 5 $@
+python Train_CoMatch_c.py --n-labeled 40 --s 4 $@
