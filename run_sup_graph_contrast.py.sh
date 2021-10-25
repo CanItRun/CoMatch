480c698d4e13172897dd8d511cfa61178af2428a
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-25 14:29:17 +0800

    /home/yanghaozhe/.lumo/experiment/sup_graph_contrast.supcontrast/211025.003.1ft

--- run_sup_graph_contrast.py.sh
+++ run_sup_graph_contrast.py.sh
@@ -1,4 +1,5 @@
 # 
 # python sup_graph_contrast.py --device=2 $@
+# python sup_graph_contrast.py --device=2 --s=1 $@
 
-python sup_graph_contrast.py --device=2 --s=1 $@
+python sup_graph_contrast.py --device=3 --s=1 $@
