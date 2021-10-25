785d294ed071b24111cf3cbe40081e977b58792d
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-25 17:56:37 +0800

    /home/yanghaozhe/.lumo/experiment/graph_comatch.comatch/211025.019.ect

--- run_graph_comatch.py.sh
+++ run_graph_comatch.py.sh
@@ -14,5 +14,6 @@
 # python graph_comatch.py --s=2 --graph_s=0 --device=5 --pretrain_path=/home/yanghaozhe/.lumo/blob/graph_comatch.comatch/211025.008.3ft/saver/model.000007.pt $@
 # python graph_comatch.py --s=2 --graph_s=0 --device=5 --pretrain_path=/home/yanghaozhe/.lumo/blob/graph_comatch.comatch/211025.008.3ft/saver/model.000007.pt --eidx=8 $@
 # python graph_comatch.py --s=2 --graph_s=0 --device=1 --pretrain_path=/home/yanghaozhe/.lumo/blob/graph_comatch.comatch/211025.013.75t/saver/model.000027.pt --eidx=27 $@
+# python graph_comatch.py --s=2 --graph_s=0 --device=1 $@
 
-python graph_comatch.py --s=2 --graph_s=0 --device=1 $@
+python graph_comatch.py --s=4 --graph_s=0 --device=1 $@
