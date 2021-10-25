65e921e5e2f26e0a8151929988c9a1236920b2b2
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-25 17:54:04 +0800

    /home/yanghaozhe/.lumo/experiment/graph_comatch.comatch/211025.016.35t

--- graph_comatch.py
+++ graph_comatch.py
@@ -436,6 +436,9 @@ class CoMatch(Trainer, MSELoss, L2Loss, callbacks.InitialCallback, callbacks.Tra
 
             anchor = batch_cosine_similarity(un_gquery, pos_memory)
             positive = batch_cosine_similarity(un_gkey, pos_memory)
+
+            print(F.cosine_similarity(anchor, positive, dim=-1))
+
             if params.graph_head:
                 anchor, positive = self.graph_head(torch.cat([anchor, positive])).chunk(2)
             # negative = batch_cosine_similarity(un_key, neg_memory)
