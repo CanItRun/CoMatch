2a54d09fad79b99809a0c483dd6cce4f28afe552
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-25 19:26:20 +0800

    /home/yanghaozhe/.lumo/experiment/sup_graph_contrast.supcontrast/211025.027.0et

--- run_sup_graph_contrast.py.sh
+++ run_sup_graph_contrast.py.sh
@@ -6,5 +6,6 @@
 # python sup_graph_contrast.py --graph_s=1 $@
 # python sup_graph_contrast.py --graph_s=1 --s=2 $@
 # python sup_graph_contrast.py --graph_s=1 --s=2 --device=1 $@
+# python sup_graph_contrast.py --graph_s=1 --s=2 --device=5 $@
 
-python sup_graph_contrast.py --graph_s=1 --s=2 --device=5 $@
+python sup_graph_contrast.py --graph_s=1 --s=1 $@
