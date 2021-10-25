4365a10b1b4a12a51d739b75392a939cbed65c29
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-25 19:56:15 +0800

    /home/yanghaozhe/.lumo/experiment/noisy_graph_contrast.supcontrast/211025.005.e5t

--- run_noisy_graph_contrast.py.sh
+++ run_noisy_graph_contrast.py.sh
@@ -1,5 +1,6 @@
 # 
 # python noisy_graph_contrast.py --device=0 --s=0 $@
 # python noisy_graph_contrast.py --device=1 --s=1 --graph_s=1 --sup_graph=False $@
+# python noisy_graph_contrast.py --device=1 --s=1 --graph_s=1 --sup_graph=True $@
 
-python noisy_graph_contrast.py --device=1 --s=1 --graph_s=1 --sup_graph=True $@
+python noisy_graph_contrast.py --device=1 --s=2 --graph_s=1 --sup_graph=True $@
