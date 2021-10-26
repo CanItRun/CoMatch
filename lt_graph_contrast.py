5861dccf50563bb84a05274fb153ed11260b6cbc
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-26 11:00:43 +0800

    /home/yanghaozhe/.lumo/experiment/lt_graph_contrast.supcontrast/211026.001.24t

--- lt_graph_contrast.py
+++ lt_graph_contrast.py
@@ -442,6 +442,11 @@ class SupContrast(Trainer, MSELoss, L2Loss, callbacks.InitialCallback, callbacks
         self.test()
         self.save_model()
 
+    def on_train_step_end(self, trainer: Trainer, func, params: Params, meter: Meter, *args, **kwargs):
+        super().on_train_step_end(trainer, func, params, meter, *args, **kwargs)
+        if params.idx % 150 == 0:
+            self.test()
+
 
 def main():
     params = ParamsType()
