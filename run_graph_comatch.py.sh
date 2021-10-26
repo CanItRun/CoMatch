3460688d956ca43e7491b2f87a2cbb18af5f2d4b
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-26 10:26:46 +0800

    /home/yanghaozhe/.lumo/experiment/graph_comatch.comatch/211026.006.a1t

--- run_graph_comatch.py.sh
+++ run_graph_comatch.py.sh
@@ -17,7 +17,7 @@
 # python graph_comatch.py --s=2 --graph_s=0 --device=1 $@
 # python graph_comatch.py --s=4 --graph_s=0 --device=1 $@
 # python graph_comatch.py --s=3 --graph_s=1 --device=3 $@
-# python graph_comatch.py --device=1 --s=3 $@
 # python graph_comatch.py --device=1 --s=1 $@
+# python graph_comatch.py --device=1 --s=21 $@
 
-python graph_comatch.py --device=1 --s=21 $@
+python graph_comatch.py --device=1 --s=3 $@
