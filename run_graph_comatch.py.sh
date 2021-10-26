31a711ab0c0e70828192f87b279bda69af94e2c6
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-26 10:27:12 +0800

    /home/yanghaozhe/.lumo/experiment/graph_comatch.comatch/211026.008.f2t

--- run_graph_comatch.py.sh
+++ run_graph_comatch.py.sh
@@ -5,7 +5,6 @@
 # python graph_comatch.py --s=0 --device=1 $@
 # python graph_comatch.py --s=1 --device=2 $@
 # python graph_comatch.py --s=1 --device=3 $@
-# python graph_comatch.py --device=3 --s=1 $@
 # python graph_comatch.py --s=3 --device=4 $@
 # python graph_comatch.py --s=3 --graph_s=1 --device=5 $@
 # python graph_comatch.py --s=0 --device=0 $@
@@ -19,5 +18,6 @@
 # python graph_comatch.py --s=3 --graph_s=1 --device=3 $@
 # python graph_comatch.py --device=1 --s=21 $@
 # python graph_comatch.py --device=1 --s=3 $@
+# python graph_comatch.py --device=1 --s=1 $@
 
-python graph_comatch.py --device=1 --s=1 $@
+python graph_comatch.py --device=3 --s=1 $@
