3f80d4395b48cf500ae2f6f4d21a16f95310d11c
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-26 21:10:38 +0800

    /home/yanghaozhe/.lumo/experiment/try_max_graph_contrast.comatch/211026.001.51t

--- run_try_max_graph_contrast.py.sh
+++ run_try_max_graph_contrast.py.sh
@@ -1,3 +1,4 @@
 # 
+# python try_max_graph_contrast.py --graph-s=1 --s=4 $@
 
-python try_max_graph_contrast.py --graph-s=1 --s=4 $@
+python try_max_graph_contrast.py --graph-s=1 --s=4 --device=3 $@
