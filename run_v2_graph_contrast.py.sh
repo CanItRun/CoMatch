31f4810f04a8ac97a01c6135d4213fc3d848fe02
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-27 14:28:49 +0800

    /home/yanghaozhe/.lumo/experiment/v2_graph_contrast.comatch/211027.003.f2t

--- run_v2_graph_contrast.py.sh
+++ run_v2_graph_contrast.py.sh
@@ -1,5 +1,6 @@
 # 
 # python v2_graph_contrast.py --device=0 --graph_s=0 --gcs_w=0.2 --avg_from=cs --q_from=prob $@
 # python v2_graph_contrast.py --device=1 --graph_s=0 --gcs_w=0.5 --avg_from=cs --q_from=prob $@
+# python v2_graph_contrast.py --device=2 --graph_s=0 --gcs_w=0.7 --avg_from=cs --q_from=prob $@
 
-python v2_graph_contrast.py --device=2 --graph_s=0 --gcs_w=0.7 --avg_from=cs --q_from=prob $@
+python v2_graph_contrast.py --device=0 --graph_s=1 --gcs_w=0.5 --avg_from=cs --q_from=prob $@
