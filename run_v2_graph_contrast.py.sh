75ff157ac5a2812fc43ca6ed312ded6ad74f2ad7
yanghaozhe <yanghaozhe@kuaishou.com>
2021-10-28 16:30:45 +0800

    /home/yanghaozhe/.lumo/experiment/v2_graph_contrast.comatch/211028.005.03t

--- run_v2_graph_contrast.py.sh
+++ run_v2_graph_contrast.py.sh
@@ -1,7 +1,6 @@
 # 
 # python v2_graph_contrast.py --device=0 --graph_s=0 --gcs_w=0.2 --avg_from=cs --q_from=prob $@
 # python v2_graph_contrast.py --device=1 --graph_s=0 --gcs_w=0.5 --avg_from=cs --q_from=prob $@
-# python v2_graph_contrast.py --device=2 --graph_s=0 --gcs_w=0.7 --avg_from=cs --q_from=prob $@
 # python v2_graph_contrast.py --device=0 --graph_s=1 --gcs_w=0.5 --avg_from=cs --q_from=prob $@
 # python v2_graph_contrast.py --device=1 --graph_s=1 --gcs_w=0.5 --avg_from=gcs --q_from=gcs $@
 # python v2_graph_contrast.py --device=1 --graph_s=1 --gcs_w=0.5 --avg_from=gcs --q_from=prob $@
@@ -31,5 +30,6 @@
 # python v2_graph_contrast.py --device=0 --graph_s=0 --gcs_w=0.1 --avg_from=cs --q_from=prob $@
 # python v2_graph_contrast.py --device=1 --graph_s=0 --gcs_w=0.2 --avg_from=cs --q_from=prob $@
 # python v2_graph_contrast.py --device=3 --graph_s=0 --gcs_w=0.5 --avg_from=cs --q_from=prob $@
+# python v2_graph_contrast.py --device=2 --graph_s=0 --gcs_w=0.5 --avg_from=cs --q_from=prob $@
 
-python v2_graph_contrast.py --device=2 --graph_s=0 --gcs_w=0.5 --avg_from=cs --q_from=prob $@
+python v2_graph_contrast.py --device=2 --graph_s=0 --gcs_w=0.7 --avg_from=cs --q_from=prob $@
